1a1255d802a00cc7c6415fc1f277eca21b21a4da
Peter Morlion <peter.morlion@gmail.com>
2018-03-19 21:44:32 +0100

    Create console app with some weird style

---
+++ StyleCopDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StyleCopDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Output1();
+            Output2();
+            Output3();
+
+            Console.ReadLine();
+        }
+
+        private static void Output1() {
+            Console.WriteLine("output 1");
+        }
+
+        private static void Output2()
+        {
+            Console.WriteLine("output 2");
+        }
+        private static void Output3()
+        {
+            Console.WriteLine("output 3");
+        }
+    }
+}
