e9bae058da1de7c4060413cde106c483eb4b9230
Peter Morlion <peter.morlion@gmail.com>
2019-04-09 10:08:22 +0200

    Change using directives placement

--- StyleCopDemo/Program.cs
+++ StyleCopDemo/Program.cs
@@ -2,14 +2,14 @@
 // Copyright (c) RedStar IT. All rights reserved.
 // </copyright>
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace StyleCopDemo
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
     /// <summary>
     /// Our program
     /// </summary>
