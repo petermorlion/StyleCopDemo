7ddf26ce6b8ac4a6147a74da56d73622118eaf48
Peter Morlion <peter.morlion@gmail.com>
2019-04-09 10:20:14 +0200

    Add class with local suppression

---
+++ StyleCopDemo/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StyleCopDemo
+{
+    class Customer
+    {
+#pragma warning disable SA1401 // Fields must be private
+        public string Name;
+#pragma warning restore SA1401 // Fields must be private
+    }
+}
